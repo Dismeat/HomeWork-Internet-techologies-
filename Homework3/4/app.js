390f0e390e6fe64380c4c515224667d413c6b5c4
Dismeat <55245442+Dismeat@users.noreply.github.com>
2021-12-13 03:30:31 +0300

    Add files via upload

---
+++ Homework3/4/app.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+  data() {
+    return {
+      currentTeg:'Нажми и измени меня',
+      currentTegFirst:'Нажми и измени меня',
+      isHidden: true
+    };
+         }
+}
+).mount('#app');
